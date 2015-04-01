ba4f099d98abbb5b3d6aca0847e51932a09c1f31
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-04-01 13:52:19 -0400

    Add missing dependencies.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang setup/infotab
 (define version "1.0")
 (define collection 'multi)
-(define deps '("sxml" "frog" "find-parent-dir"))
+(define deps '("sxml" "frog" "find-parent-dir" "base" "scribble-lib"))
