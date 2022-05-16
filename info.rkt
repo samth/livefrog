b6a51c11cd4a4a7bb34246ae8736f51abc74307a
Sam Tobin-Hochstadt <samth@indiana.edu>
2022-05-16 16:49:20 -0400

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,4 +1,6 @@
 #lang setup/infotab
 (define version "1.0")
 (define collection 'multi)
-(define deps '("sxml" "frog" "find-parent-dir"))
+(define deps '("base"
+               "scribble-lib"
+               "sxml" "frog" "find-parent-dir"))
